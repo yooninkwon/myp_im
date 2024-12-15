1ce9717ad0a180064c5440e9f90c64562b15fb02
yooninkwon <yooninkwon@naver.com>
2024-12-15 23:51:16 +0900

    회원가입 id 중복값 확인하기 >jpa활용

---
+++ src/main/java/com/tech/im/repository/ImUserRepository.java
@@ -0,0 +1,11 @@
+package com.tech.im.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tech.im.domain.ImUser;
+
+public interface ImUserRepository extends JpaRepository<ImUser, String>{
+
+	boolean existsByUserId(String userId);
+
+}
