be26ec96dce1b1fad7e0cff35d3333b550a30781
yooninkwon <yooninkwon@naver.com>
2024-12-15 10:14:54 +0900

    개인프로젝트 1 시작

---
+++ src/main/java/com/tech/im/MyprjImApplication.java
@@ -0,0 +1,13 @@
+package com.tech.im;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyprjImApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyprjImApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech/im/MyprjImApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.im;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprjImApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
