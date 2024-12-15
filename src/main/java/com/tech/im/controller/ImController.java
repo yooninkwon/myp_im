e0172acdbbbaabaa98665dbf6dd31f64d4093ced
yooninkwon <yooninkwon@naver.com>
2024-12-15 20:45:29 +0900

    기본 view 세팅 로그인창 > 회원가입 폼 view 완료

--- src/main/java/com/tech/im/controller/ImController.java
+++ src/main/java/com/tech/im/controller/ImController.java
@@ -11,4 +11,34 @@ public class ImController {
 		
 		return "index";
 	}
+	@RequestMapping("/who")
+	public String who() {
+		
+		return "who";
+	}
+	@RequestMapping("/life")
+	public String life() {
+		
+		return "life";
+	}
+	@RequestMapping("/worry")
+	public String worry() {
+		
+		return "worry";
+	}
+	@RequestMapping("/secret")
+	public String secret() {
+		
+		return "secret";
+	}
+	@RequestMapping("/free")
+	public String free() {
+		
+		return "free";
+	}
+	@RequestMapping("/myPage")
+	public String myPage() {
+		
+		return "myPage";
+	}
 }
