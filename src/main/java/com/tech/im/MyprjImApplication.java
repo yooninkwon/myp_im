2edb02e6920a48216706375a7c92d6753a62af3f
yooninkwon <yooninkwon@naver.com>
2024-12-21 20:50:06 +0900

    작업> 인증번호 세션에 저장 후 유저 이메일 인증 번호 일치 확인 기능 httpsession -> redis 세션타임아웃 상세 설정으로 인해 redis 사용 수정> 인증번호 메일 발송 비동기 처리

--- src/main/java/com/tech/im/MyprjImApplication.java
+++ src/main/java/com/tech/im/MyprjImApplication.java
@@ -2,6 +2,7 @@ package com.tech.im;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 public class MyprjImApplication {
